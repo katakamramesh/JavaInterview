f8db0578782d74d6283621fbae2e51fe4e8e88e3
Ramesh Katakam <katakamramesh52@gmail.com>
2025-07-18 21:04:32 +0530

    part time

---
+++ CustomFunctionalInterface.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+@FunctionalInterface
+public interface CustomFunctionalInterface {
+    String method1(String a, String b, String c);
+}
+
+class Implementation {
+
+
+    public static List<String> ll = List.of("sample1", "sample2", "sample3");
+
+    public static void main(String s[]){
+        System.out.println(ll.stream().reduce((a,b) -> {
+            String combined = a+b;
+            System.out.println(combined);
+            return combined;
+        }));
+    }
+
+}
+
